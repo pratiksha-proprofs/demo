e0196b00e21fad20b12ebb809e6fbd6829a4b2bb
Pratiksha Singh <pratiksha@proprofs.com>
2023-01-17 17:09:43 +0530

    jira

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,13 +2,17 @@ class HomesController < ApplicationController
 	def index
 		options = {
 		  :username     => 'pratiksha@proprofs.com',
-		  :password     => 'zd5OjylIK6M4cKJSp1hND180',
+		  :password     => 'SQNfIVcTt5eCvBzKXJIJBFF2',
 		  :site         => 'https://integration-test12.atlassian.net:443/',
 		  :context_path => '',
 		  :auth_type    => :basic
 		}
 	    client = JIRA::Client.new(options)
-	    issue = client.Issue.build
-	    issue.save!({ "fields": { "project": { "key": "NEW" }, "summary": "Issue check", 'description': 'Look into this one', "issuetype": { "name": "Task" } } })
+	    p = client.Project.find("10001")
+	    p.issues.each do |i|
+	    	binding.pry
+	    end
+	    # issue = client.Issue.build
+	    # issue.save!({ "fields": { "project": { "key": "NEW" }, "summary": "New Summary", 'description': 'Servay response', 'assignee': "Aarif Hussain",  "issuetype": { "name": "Task" } } })
 	end
 end
